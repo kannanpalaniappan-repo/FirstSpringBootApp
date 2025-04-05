c297c02ddddce11517581fe0c12ec0c3c559960e
kannanpalaniappan-repo <kannanpalaniappan2001@gmail.com>
2025-04-05 21:37:03 +0530

    Getting Json Response

--- src/main/java/com/springdev/FirstSpring/HelloController.java
+++ src/main/java/com/springdev/FirstSpring/HelloController.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-    public String hello(){
-        return "Hello World!";
+    public HelloResponse hello(){
+        return new HelloResponse("Hello World!");
     }
     @PostMapping("/hello")
-    public String helloPost(@RequestBody String name){
-        return "Hello, "+name+" !";
+    public HelloResponse helloPost(@RequestBody String name){
+        return new HelloResponse("Hello, "+name+" !");
     }
 }
