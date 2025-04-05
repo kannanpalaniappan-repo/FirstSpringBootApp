88f5ff95fb1f60ea9a8cf0739bcd85431b3ee03c
kannanpalaniappan-repo <kannanpalaniappan2001@gmail.com>
2025-04-05 20:41:38 +0530

    Creating a Post Request

--- src/main/java/com/springdev/FirstSpring/HelloController.java
+++ src/main/java/com/springdev/FirstSpring/HelloController.java
@@ -1,6 +1,8 @@
 package com.springdev.FirstSpring;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +10,9 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "Hello World!";
+    }
+    @PostMapping("/hello")
+    public String helloPost(@RequestBody String name){
+        return "Hello, "+name+" !";
     }
 }
